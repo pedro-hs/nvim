148a641d5a457995086f8723da8f6e0e22c01108
pedrosantosarruda <pedro.arruda@pisomtech.com.br>
2021-03-21 03:57:51 -0300

    Test install script

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt install silversearcher-ag neovim python3-pip npm
 mkdir autoload
 cd autoload
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vi -c PlugInstall +qall
 
 pip3 install flake8 isort pylint autopep8 pynvim jedi
 npm install --global prettier
