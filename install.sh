5b9551a88fa38cbbc439e04dc9da7729b5a3e70f
Pedro Santos Arruda <42540954+pedro-hs@users.noreply.github.com>
2021-03-21 03:48:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ### BEFORE
 sudo apt install silversearcher-ag neovim python3-pip npm
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 pip3 install flake8 isort pylint autopep8 pynvim jedi
 npm install --global prettier
