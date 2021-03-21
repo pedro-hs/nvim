3b94887770f7d1709cb08cc412716675b8215965
pedrosantosarruda <pedro.arruda@pisomtech.com.br>
2021-03-21 17:55:16 -0300

    Improva install scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-### BEFORE
+### RUN
+# cd ~/.config
+# sudo rm -rf nvim
+# git clone https://github.com/pedro-hs/nvim.git
+# cd nvim
+# sudo sh install.sh && vi -c PlugInstall +qall
+
 sudo apt install silversearcher-ag neovim python3-pip npm
 
 mkdir autoload
@@ -10,22 +16,22 @@ npm install --global prettier
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
 unzip DroidSansMono.zip -d ~/.fonts
-fc-cache -fv && vi -c PlugInstall +qall
+fc-cache -fv
+rm -f DroidSansMono.zip
 
-### AFTER
-# :PlugInstall
+### AFTER INSTALL
 ## COMFORTABLE MOTION VIM (change)
 # nnoremap <silent> <a-j> :call comfortable_motion#flick(100)<cr>
 # nnoremap <silent> <a-k> :call comfortable_motion#flick(-100)<cr>
-#
+
 ## BUFTABLINE + DEVICONS (change)
 # replace all s:switch_buffer to buftabline#switch_buffer
 # let tab.label = tab.path[tab.sep + 1:] . ' ' . WebDevIconsGetFileTypeSymbol(tab.path)
-#
+
 ## VIMSPECTOR (run)
 # :VimspectorInstall debugpy
 # :VimspectorInstall debugger-for-chrome
-#
+
 ## VIM VISUAL MULTI (comment)
 # autoload/vm.vim
 #     call s:V.Funcs.msg('Exited Visual-Multi.')
@@ -33,6 +39,6 @@ fc-cache -fv && vi -c PlugInstall +qall
 #     call s:V.Funcs.msg('VM has started with warnings. :VMDebug for more info')
 # autoload/vm/funcs.vim
 #     silent! noautocmd setlocal statusline=%!vm#themes#statusline()
-#
+
 # ALE (comment)
 # execute 'echoerr ''The file was changed before fixing finished'''
