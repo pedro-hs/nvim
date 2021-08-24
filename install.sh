876958f845ead4ef9beb3b7d7e5504e9c6ac4f85
pedro-hs <pedrosantosarruda@outlook.com>
2021-08-23 21:26:41 -0300

    Add neovim to npm installation

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ cd autoload
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 pip3 install flake8 isort pylint autopep8 pynvim jedi
-sudo npm install --global prettier
+sudo npm install --global prettier neovim
 
 ### AFTER INSTALL
 # ALE (comment)
