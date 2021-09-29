2fb453afed1ebaf0c789b60649227b1dec200d85
Pedro Santos Arruda <42540954+pedro-hs@users.noreply.github.com>
2021-09-29 03:14:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # cd nvim
 # sh install.sh && vi -c PlugInstall +qall
 
-sudo apt install -y silversearcher-ag neovim python3-pip nodejs node-gyp npm
+sudo apt install -y silversearcher-ag neovim python3-pip nodejs node-gyp nodejs-dev npm
 
 mkdir autoload
 cd autoload
